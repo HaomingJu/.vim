0e54a0fcccdd84b7c55aa6ce6641c66d536f2341
Haoming.Ju <JuHaoming@Trunk.tech>
2019-08-02 15:29:22 +0800

    纠正ycm补全文件格式 & 我现在也不知道哪种是正确的了 : (

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -27,20 +27,13 @@ flags = [
 '-Werror',
 '-fexceptions',
 '-DNDEBUG',
-# THIS IS IMPORTANT! Without a "-std=<something>" flag, clang won't know which
-# language to use when compiling headers. So it will guess. Badly. So C++
-# headers will be compiled as C headers. You don't want that so ALWAYS specify
-# a "-std=<something>".
-# For a C project, you would set this to something like 'c99' instead of
-# 'c++11'.
 '-std=c++11',
-# ...and the same thing goes for the magic -x option which specifies the
-# language that the files to be compiled are written in. This is mostly
-# relevant for c++ headers.
-# For a C project, you would set this to 'c' instead of 'c++'.
 '-x',
 'c++',
-'-I/usr/include/c++/7.3.0',
+'-I',
+'/usr/include/c++/5',
+'-I',
+'/usr/include/glog',
 ]
 
 
--- vimrc
+++ vimrc
@@ -127,7 +127,7 @@ let g:ycm_autoclose_preview_window_after_insertion=1
 let g:ycm_confirm_extra_conf=0                                          " 打开vim时不再询问是否加载ycm_extra_conf.py配置
 let g:ycm_collect_identifiers_from_tag_files = 1                        " 使用ctags生成的tags文件
 let g:ycm_key_list_previous_completion = ['<c-p>', '<Up>']
-let g:ycm_global_ycm_extra_conf='/home/haoming/.vim/.ycm_extra_conf.py'
+let g:ycm_global_ycm_extra_conf='~/.vim/.ycm_extra_conf.py'
 let g:ycm_show_diagnostics_ui=0
 "CtrlSpace 插件配置
 set nocompatible
