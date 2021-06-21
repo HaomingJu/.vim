9860daefaeaff2c8c5c633c923596560d8d77cca
HaomingJu <juhaoming@trunk.tech>
2021-06-21 14:10:45 +0800

    use default theme

--- env/zsh/oh_my_zsh.zsh
+++ env/zsh/oh_my_zsh.zsh
@@ -2,7 +2,7 @@ plugins=(git colored-man-pages z cp extract history docker docker-compose dash)
 
 export ZSH=$HOME/.oh-my-zsh
 export TERM=xterm-256color
-ZSH_THEME="apple"
+ZSH_THEME="robbyrussell"
 HIST_STAMPS="mm/dd/yyyy"
 
 # 不保存重复的历史记录项
